1384b4a91791d8b98f970e63cf4c4f5f6ad4cd9c
“xiu0327” <codinging0326@gmail.com>
2022-07-16 05:12:28 +0000

    22.07.16 commit

--- back/lists.py
+++ back/lists.py
@@ -1,22 +1,21 @@
 from flask import request
 from flask_restx import Resource, Api, Namespace, fields
-from func import print_json, userLoanlist
+from func import print_json, userLoanlist, sns_blog_keyword
 
 List = Namespace('List', description="각종 목록을 가져오기 위한 API (ex: 도서관 목록, 키워드, 사용자 대출 목록 등)")
 
 @List.route('/lib')
-class ListGet(Resource):
+class Library(Resource):
     def get(self):
         """전국 도서관 목록을 가져오는 API"""
         path = "/home/ubuntu/data/libraryList.json"
         return print_json.get_data(path)
 
-@List.route('/keyword')
-class ListGet(Resource):
-    def get(self):
+@List.route('/keyword/<string:userid>')
+class Keyword(Resource):
+    def get(self, userid):
         """사용자 블로그에서 추출한 키워드 목록 가져오는 API"""
-        path = "/home/ubuntu/data/keyword_test.json"
-        return print_json.get_data(path)
+        return sns_blog_keyword.keywords(userid)
 
 @List.route('/user', methods=['POST'])
 class ListPost(Resource):
